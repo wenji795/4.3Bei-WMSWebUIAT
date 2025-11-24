2fcf1e16f8620ec9c2f62539822b51b21b18aeef
Wenji Shen <swjoanna795@gmail.com>
2025-11-24 22:13:13 +1300

    add demo, but 数据库中的存储过程没有权限创建

---
+++ demo.py
@@ -0,0 +1,36 @@
+import pymysql
+
+connection = pymysql.connect(
+    host='60.204.225.104',
+    port=3306,
+    user='student_xiaobei',
+    password='xiaobeiup',
+    database='wms',
+    charset='utf8mb4',
+    autocommit=True
+)
+
+
+
+with connection.cursor() as cursor:
+    # 先不用存储过程，直接查表
+    cursor.execute("SELECT * FROM sys_user;")
+    result = cursor.fetchall()
+    for row in result:
+        print(row)
+
+connection.close()
+
+
+
+# with connection.cursor() as cursor:
+#     #调用存储过程
+#     cursor.callproc('TEST')
+#
+#     #获取结果
+#     result = cursor.fetchall()
+#     for row in result:
+#         print(row)
+#
+#
+# connection.close()

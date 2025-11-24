9fd4a02ea6dc7e7f0ee05b29a45ad3fdeb35e036
Wenji Shen <swjoanna795@gmail.com>
2025-11-24 23:36:31 +1300

    delete:用于测试的demo

--- demo.py
+++ demo.py
@@ -1,25 +1,25 @@
-import pymysql
-
-connection = pymysql.connect(
-    host='60.204.225.104',
-    port=3306,
-    user='student_xiaobei',
-    password='xiaobeiup',
-    database='wms',
-    charset='utf8mb4',
-    autocommit=True
-)
-
-
-
-with connection.cursor() as cursor:
-    # 先不用存储过程，直接查表
-    cursor.execute("SELECT * FROM sys_user;")
-    result = cursor.fetchall()
-    for row in result:
-        print(row)
-
-connection.close()
+# import pymysql
+#
+# connection = pymysql.connect(
+#     host='60.204.225.104',
+#     port=3306,
+#     user='student_xiaobei',
+#     password='xiaobeiup',
+#     database='wms',
+#     charset='utf8mb4',
+#     autocommit=True
+# )
+#
+#
+#
+# with connection.cursor() as cursor:
+#     # 先不用存储过程，直接查表
+#     cursor.execute("SELECT * FROM sys_user;")
+#     result = cursor.fetchall()
+#     for row in result:
+#         print(row)
+#
+# connection.close()
 
 
 

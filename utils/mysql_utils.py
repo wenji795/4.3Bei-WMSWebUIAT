35fc12bcc24c32ad8a7ba73fa25c02379462fd42
Wenji Shen <swjoanna795@gmail.com>
2025-11-25 13:01:24 +1300

    feat: 加了一个自动执行sql文件的函数，execute_sql_file()。因为权限问题没法run，先commit一下

--- utils/mysql_utils.py
+++ utils/mysql_utils.py
@@ -1,4 +1,7 @@
+#mysql_utils.py
+
 import pymysql
+import sqlparse
 
 from config.config import *
 
@@ -16,13 +19,42 @@ def get_connection():
     return connection
 
 
+#执行一个 SQL 文件，然后调用其中的存储过程
+#因为在自动化测试里，经常需要初始化数据，清理旧数据
+def execute_sql_file(filepath, procedure_name):
+    # 1. 获取数据库连接（从 get_connection()）
+    connection = get_connection()
 
+    # 2. 读取 SQL 文件
+    with open(filepath, 'r', encoding='utf-8') as f:
 
-with connection.cursor() as cursor:
-    # 先不用存储过程，直接查表
-    cursor.execute("SELECT * FROM sys_user;")
-    result = cursor.fetchall()
-    for row in result:
-        print(row)
+        # 3. 用 sqlparse 拆分 SQL 语句（智能识别 BEGIN…END）
+        statements = sqlparse.split(f.read())
+        # print(statements)
+        # 比如 test.sql 里可能有：
+        #   DROP PROCEDURE IF EXISTS TEST;
+        #   CREATE PROCEDURE TEST() BEGIN ... END;
+        # 就会被拆成 2 条 SQL 语句
 
-connection.close()
+    # 4. 开始执行 SQL 语句
+    with connection.cursor() as cursor:
+
+        # 5. 遍历所有 SQL 语句并执行
+        for statement in statements:
+            cursor.execute(statement)
+            # 注意：如果用户没有创建存储过程权限，会报 1044 错
+
+        # 6. 执行文件里定义的存储过程
+        cursor.callproc(procedure_name)
+
+        # 7. 如需查看执行结果可以 fetchall()
+        result = cursor.fetchall()
+        for row in result:
+            print(row)
+
+    # 8. 关闭数据库连接
+    connection.close()
+
+if __name__ == '__main__':
+    execute_sql_file('../config/test.sql','TEST')
+
